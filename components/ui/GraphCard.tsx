b7fd4f7687c83629f5612f9ee2912c8bbd3df366
YoufuYAN <arnoldyyf@gmail.com>
2023-11-27 23:19:01 -0600

    Refactor GraphCard component

--- components/ui/GraphCard.tsx
+++ components/ui/GraphCard.tsx
@@ -2,6 +2,11 @@
 import React, { useEffect, useRef } from 'react'
 import * as d3 from 'd3'
 import { GraphData } from '@/lib/types'
+interface SimulationNode extends d3.SimulationNodeDatum {
+  id: string
+  label: string
+  group: string
+}
 
 export const GraphCard: React.FC<{ graphData: GraphData }> = ({
   graphData
@@ -14,24 +19,25 @@ export const GraphCard: React.FC<{ graphData: GraphData }> = ({
     const width = svgElement.getBoundingClientRect().width
     const height = svgElement.getBoundingClientRect().height
     const { nodes, edges } = graphData
-    // let simulation = d3
-    //   .forceSimulation(nodes)
-    //   .force(
-    //     'link',
-    //     d3.forceLink(edges).id((d: { id: any }) => d.id)
-    //   )
-    //   .force('charge', d3.forceManyBody())
-    //   .force('center', d3.forceCenter(300, 300)) // Assuming the width and height of the SVG are 600x600
+    // Ensure nodes are compatible with SimulationNodeDatum
+    const simulationNodes: SimulationNode[] = nodes.map(node => ({
+      ...node,
+      id: node.id.toString()
+    }))
+    const simulationEdges = edges.map(edge => ({
+      ...edge,
+      source: edge.source.toString(),
+      target: edge.target.toString()
+    }))
+    // Extend your Node type to include properties required by SimulationNodeDatum
 
-    // const svg = d3.select(svgRef.current)
-
-    // // Clear the SVG to prevent duplication
-    // svg.selectAll('*').remove()
     let simulation = d3
-      .forceSimulation(nodes)
+      .forceSimulation(simulationNodes)
       .force(
         'link',
-        d3.forceLink(edges).id((d: { id: any }) => d.id)
+        d3
+          .forceLink(simulationEdges)
+          .id((d: d3.SimulationNodeDatum) => (d as any).id)
       )
       .force('charge', d3.forceManyBody())
       .force('center', d3.forceCenter(width / 2, height / 2))
@@ -47,78 +53,69 @@ export const GraphCard: React.FC<{ graphData: GraphData }> = ({
       .attr('stroke', '#999')
       .attr('stroke-opacity', 0.6)
       .selectAll('line')
-      .data(edges)
+      .data(simulationEdges)
       .join('line')
-      .attr('stroke-width', (d: { value: number }) => Math.sqrt(d.value))
+      .attr('stroke-width', (d: any) => Math.sqrt(d.value))
 
     // Draw the nodes (circles)
     const node = svg
       .append('g')
       .attr('stroke', '#fff')
       .attr('stroke-width', 1.5)
-      .selectAll('circle')
-      .data(nodes)
+      .selectAll<SVGCircleElement, SimulationNode>('circle')
+      .data(simulationNodes)
       .join('circle')
       .attr('r', 5)
-      .attr('fill', (d: { group: any }) => d.group)
-
-    // Add drag behavior to nodes
-    node.call(drag(simulation))
+      .attr('fill', (d: any) => d.group)
 
     // Define what to do on each tick
     simulation.on('tick', () => {
-      // Update link positions
       link
-        .attr('x1', (d: { source: { x: any } }) => d.source.x)
-        .attr('y1', (d: { source: { y: any } }) => d.source.y)
-        .attr('x2', (d: { target: { x: any } }) => d.target.x)
-        .attr('y2', (d: { target: { y: any } }) => d.target.y)
-
-      // Update node positions
-      node.attr('cx', (d: { x: any }) => d.x).attr('cy', (d: { y: any }) => d.y)
+        .attr('x1', (d: any) => d.source.x)
+        .attr('y1', (d: any) => d.source.y)
+        .attr('x2', (d: any) => d.target.x)
+        .attr('y2', (d: any) => d.target.y)
+      node.attr('cx', (d: any) => d.x).attr('cy', (d: any) => d.y)
     })
 
-    // Drag functions for interactivity
-    function drag(simulation: {
-      alphaTarget: (arg0: number) => {
-        (): any
-        new (): any
-        restart: { (): void; new (): any }
-      }
-    }) {
-      function dragstarted(event: {
-        active: any
-        subject: { fx: any; x: any; fy: any; y: any }
-      }) {
+    // Modify the drag function
+    // Modify the drag function to match the SVGCircleElement type
+    function drag(simulation: d3.Simulation<SimulationNode, undefined>) {
+      function dragstarted(
+        event: d3.D3DragEvent<SVGCircleElement, SimulationNode, SimulationNode>,
+        d: SimulationNode
+      ) {
         if (!event.active) simulation.alphaTarget(0.3).restart()
-        event.subject.fx = event.subject.x
-        event.subject.fy = event.subject.y
+        d.fx = d.x
+        d.fy = d.y
       }
 
-      function dragged(event: {
-        subject: { fx: any; fy: any }
-        x: any
-        y: any
-      }) {
-        event.subject.fx = event.x
-        event.subject.fy = event.y
+      function dragged(
+        event: d3.D3DragEvent<SVGCircleElement, SimulationNode, SimulationNode>,
+        d: SimulationNode
+      ) {
+        d.fx = event.x
+        d.fy = event.y
       }
 
-      function dragended(event: {
-        active: any
-        subject: { fx: null; fy: null }
-      }) {
+      function dragended(
+        event: d3.D3DragEvent<SVGCircleElement, SimulationNode, SimulationNode>,
+        d: SimulationNode
+      ) {
         if (!event.active) simulation.alphaTarget(0)
-        event.subject.fx = null
-        event.subject.fy = null
+        d.fx = null
+        d.fy = null
       }
 
       return d3
-        .drag()
+        .drag<SVGCircleElement, SimulationNode, SimulationNode>()
         .on('start', dragstarted)
         .on('drag', dragged)
         .on('end', dragended)
     }
+
+    // Add drag behavior to nodes
+    node.call(drag(simulation))
   }, [graphData])
 
   return (
