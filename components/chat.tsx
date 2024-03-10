2c44b685b16262ff1041a31b9388d8088502acd0
wangqianwen0418 <wangqianwen0418@163.com>
2024-03-10 00:47:35 -0600

    fix type

--- components/chat.tsx
+++ components/chat.tsx
@@ -467,7 +467,7 @@ export function Chat({
                       height: 'calc(65vh - 1rem)'
                     }}
                   >
-                    { (isLoadingBackendData || isLoading ) ?? (
+                    { (isLoadingBackendData || isLoading ) && (
                       <div className="absolute inset-0 bg-white bg-opacity-50 flex justify-center items-center z-10">
                         <Spinner color="blue" />
                       </div>
