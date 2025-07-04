c8559cf2a3789950c3a43052f9d2885f972d6373
Mauricio Villavicencio <mauriciov@Mauricios-MacBook-Air.local>
2025-07-03 19:07:38 -0500

    Added animations

--- src/components/vis-flow/customNode.tsx
+++ src/components/vis-flow/customNode.tsx
@@ -1,55 +1,47 @@
 import { NodeProps, NodeToolbar, Handle, Position } from "reactflow";
 import { FC } from "react";
 import { Button } from "../ui/button";
+import { motion } from "framer-motion";
 
 const CustomNode: FC<NodeProps> = ({
     sourcePosition,
     targetPosition,
     data
 }) => {
-    // console.log("node data", data)
     return (
         <>
-            <div
+            <motion.div
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.7 }}
                 style={{
-                overflow: 'hidden',
-                borderColor: '#1A192B',
-                borderWidth: '1px',
-                borderStyle: 'solid',
-                borderRadius: '5px',
-                boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.07)',
-                padding: '10px',
-                minWidth: '150px',
-                minHeight: '40px',
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                gap: '10px',
-                boxSizing: 'border-box'
+                    overflow: 'hidden',
+                    borderColor: '#1A192B',
+                    borderWidth: '1px',
+                    borderStyle: 'solid',
+                    borderRadius: '5px',
+                    boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.07)',
+                    padding: '10px',
+                    minWidth: '150px',
+                    minHeight: '40px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '10px',
+                    boxSizing: 'border-box'
                 }}
             >
-                {/* <div className="text-xs">Tell me more about:</div> */}
                 <NodeToolbar>
-                    <Button
-                        // key={recommendation.id}
-                        variant="outline"
-                        className="mr-2" // Add some spacing between buttons
-                        // onClick={() => handleRecommendationClick(recommendation)}
-                        // title={recommendation.text}
-                    >
-                        {/* {recommendation.text
-                        .replace(data.label, '')
-                        .replace(' and ', '')} */}
+                    <Button variant="outline" className="mr-2">
                         recommendation
                     </Button>
-
                 </NodeToolbar>
                 <div className="reactflow">{data.label}</div>
                     
                 <Handle type="target" position={targetPosition || Position.Top} />
                 <Handle type="source" position={sourcePosition || Position.Bottom} />
-            </div>
+            </motion.div>
         </>
     )
 }
